94af3568c11bd277fbb7999611dea75fc12c34e2
anurag653 <anuragsinghaaa7@gmail.com>
2024-07-30 14:32:01 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const hamburger_menu = document.querySelector(".hamburgermenu");
-const container = document.querySelector(".container");
-
-hamburger_menu.addEventListener("click", () => {
-    container.classList.toggle("active");
-});
